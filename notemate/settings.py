81708f56d1a9a0420e3bed852471a4a70da95cf2
shashwat2645 <singhshashwat133@gmail.com>
2026-04-18 22:26:24 +0530

    Add Render domain to ALLOWED_HOSTS

--- notemate/settings.py
+++ notemate/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') + ['.render.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,notemate-uyhf.onrender.com').split(',')
 
 
 INSTALLED_APPS = [
