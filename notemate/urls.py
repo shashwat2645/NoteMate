d4b9919fe341b9b3f792f133fc72b5b96ba26907
shashwat2645 <singhshashwat133@gmail.com>
2026-04-19 14:50:16 +0530

    Clean up for deploy

--- notemate/urls.py
+++ notemate/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/login/', admin.site.login),
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('notes/', include('notes.urls')),
