253e7f35652be3add480661866283f56de2f55cc
shashwat2645 <singhshashwat133@gmail.com>
2026-04-20 12:18:43 +0530

    Fix import order in WSGI configuration for consistency

--- notemate/wsgi.py
+++ notemate/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notemate.settings')
 
 application = get_wsgi_application()
