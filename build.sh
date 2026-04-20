39ce21d779947e2f6bef5dfa971c2ae028172299
shashwat2645 <singhshashwat133@gmail.com>
2026-04-20 12:05:06 +0530

    Add DATABASE_URL check and update migration command in WSGI and build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-python3 -c "import os; print('DATABASE_URL set:', bool(os.getenv('DATABASE_URL')))"
 python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+python3 manage.py migrate --noinput
--- notemate/wsgi.py
+++ notemate/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notemate.settings')
+
+from django.conf import settings
+from django.db import connection
+
+if os.getenv('DATABASE_URL'):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1 FROM django_migrations LIMIT 1")
+    except:
+        execute_from_command_line(['manage.py', 'migrate', '--noinput'])
 
 application = get_wsgi_application()
